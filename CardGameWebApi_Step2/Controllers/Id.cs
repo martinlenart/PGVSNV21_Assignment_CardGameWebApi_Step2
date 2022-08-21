b00ab53e94c623df600697fccbcc5b62cb55b3e6
Martin <martin_lenart@icloud.com>
2022-08-21 14:39:16 +0200

    initial commit

---
+++ CardGameWebApi_Step2/Controllers/Id.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CardGameWebApi.Models;
+
+//for reading assembly information
+using System.Reflection;
+using System.Configuration;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.mvc.controllerbase.createdatroute?view=aspnetcore-6.0
+
+
+namespace CardGameWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]     //All actions (methods) in the controller will be named Id. They must differentiate in Url
+    public class IdController : ControllerBase
+    {
+        //api/id
+        [HttpGet(Name = nameof(WhoAmI))]
+        public IEnumerable<string> WhoAmI()
+        {
+            
+            Assembly asmExe = Assembly.GetExecutingAssembly();
+            var asn = asmExe.GetName();
+            var version = asn.Version;
+
+            /*
+            //If you want to explore Assembly information
+            IEnumerable<CustomAttributeData> assemblyAttributes = asmExe.CustomAttributes;
+
+            Console.WriteLine("\n\nAssembly Attributes:");
+            foreach (CustomAttributeData assemblyAttribute in assemblyAttributes)
+            {
+                Type attributeType = assemblyAttribute.AttributeType;
+                Console.WriteLine($"\nArguments of attribute type: {attributeType}");
+                IList<CustomAttributeTypedArgument> arguments = assemblyAttribute.ConstructorArguments;
+                foreach (CustomAttributeTypedArgument arg in arguments)
+                {
+                    Console.WriteLine($"   {arg.Value}");
+                }
+            }
+            */
+
+
+            return new string[] { asn.Name, version.ToString(), "An example of how to implement a CardGame as a WebApi", Url.Link(nameof(WhoAmI), null) };
+        }
+
+        //api/id/{someParam}/?authordetails={authordetails}
+        [HttpGet("{someParam}", Name = nameof(Author))]            //"{someParam}" differentiates the actions, which otherwise have same url
+        public IEnumerable<string> Author(string authorDetails)
+        {
+            if (authorDetails.ToLower().Trim() == "name")
+                return new string[] { "Martin Lenart" };
+
+            return new string[] { "Martin Lenart", "Backvagen1, 184 42 Akersberga", Url.Link(nameof(Author), null) };
+        }
+
+        // /Author
+        [Route("/Developer")]
+        [HttpGet]
+        public IEnumerable<string> Developer() => Author("name");
+    }
+}
+
---
+++ CardGameWebApi_Step2/Controllers/card.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CardGameWebApi.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+//https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/routing?view=aspnetcore-6.0
+//https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.mvc.httpgetattribute?view=aspnetcore-6.0
+//https://docs.microsoft.com/en-us/dotnet/api/system.net.httpstatuscode?view=net-6.0
+//https://docs.microsoft.com/en-us/aspnet/core/web-api/action-return-types?view=aspnetcore-6.0#iactionresult-type
+//https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.mvc.controllerbase.ok?view=aspnetcore-6.0
+
+
+namespace CardGameWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]  //All actions (methods) in the controller will be named as the method
+                                          //allows me to use simple   [HttpGet]
+                                          
+    public class CardGameController : ControllerBase
+    {
+        static bool gameStarted = false;
+        static string gameTypeRunning = null;
+
+        //GET: api/startgame?gametype={gameType}
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> StartGame(string gameType)
+        {
+            if (gameStarted)
+            {
+                return BadRequest($"Game of type {gameTypeRunning} is already running");
+            }
+            if (gameType.ToLower().Trim() == "highcard")
+            {
+                gameStarted = true;
+                gameTypeRunning = gameType.ToLower().Trim();
+                return Ok($"Game of type {gameTypeRunning} started");
+            }
+
+            return BadRequest($"Game of type {gameType} does not exist");
+        }
+
+        
+        //GET: api/endgame
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> EndGame()
+        {
+            if (gameStarted)
+            {
+                var gtr = gameTypeRunning;
+                gameTypeRunning = null;
+                gameStarted = false;
+
+                return Ok($"Game of type {gtr} ended");
+            }
+
+            return BadRequest($"No game is running");
+        }
+
+        
+        //GET: api/dealcard
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(PlayingCard))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> DealCard()
+        {
+            if (!gameStarted)
+                return BadRequest($"No game is running");
+
+            return Ok(PlayingCard.CreateRandom());
+        }
+
+        
+        //GET: api/dealcards/?nrOfCard={nrOfCards}
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<PlayingCard>))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> DealCards(string nrOfCards)
+        {
+            if (!gameStarted)
+                return BadRequest($"No game is running");
+
+            int _nrOfCards;
+            if (int.TryParse(nrOfCards, out _nrOfCards) && _nrOfCards > 1 && _nrOfCards <= 5)
+            {
+                //deal the cards
+                var hand = new List<PlayingCard>();
+                for (int i = 0; i < _nrOfCards; i++)
+                {
+                    hand.Add(PlayingCard.CreateRandom());
+                }
+
+                return Ok(hand);
+            }
+
+            //indicate error in nrOfCards
+            return BadRequest($"{nrOfCards} is not a valid amount of cards");
+        }
+
+
+        //POST: api/winningcard   
+        //Body: List<PlayingCard> in Json
+        [HttpPost]                          //Needs to be PUT or POST as I have a request Body
+        [ProducesResponseType(200, Type = typeof(PlayingCard))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> WinningCard([FromBody] List<PlayingCard> hand)
+        {
+            if (!gameStarted)
+                return BadRequest($"No game is running");
+
+            if (hand.Count <1)
+                return BadRequest($"No cards to evaluate");
+
+            //respond with winning card 
+            return Ok(hand[0]);
+        }
+    }
+}
+
